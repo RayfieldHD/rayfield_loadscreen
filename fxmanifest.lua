02ed2aa6232d06c6db36dd941dae774f4d4ce626
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-06-02 00:39:42 +0100

    Created fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5' 
+
+loadscreen_manual_shutdown 'yes'
+loadscreen 'index.html'
+client_script "client.lua"
+
+author 'Rayfield & aidanohartt'
+version '1.0.0'
+description 'LABRP Load Screen'
+
+files {
+    'index.html',
+    'main.css'
+}
